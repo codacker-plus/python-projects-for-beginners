174dffa1c3d3d9b9eba451b86b7771b2ea16060c
codakcer <158773902+codacker-plus@users.noreply.github.com>
2025-04-30 10:46:06 +0330

    Create 10_simple_chatbot.py

---
+++ 10_simple_chatbot.py
@@ -0,0 +1,11 @@
+while True:
+    msg = input("You: ").lower()
+    if 'hello' in msg:
+        print("Bot: Hi there!")
+    elif 'how are you' in msg:
+        print("Bot: I'm just code, but I'm fine!")
+    elif 'bye' in msg:
+        print("Bot: Goodbye!")
+        break
+    else:
+        print("Bot: I don't understand.")
